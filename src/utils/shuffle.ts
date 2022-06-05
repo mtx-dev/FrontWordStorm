17c53ab86dd9c644963c2a3cafb102c0da680c47
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-06-05 23:24:07 +0300

    Fix bug em=pty array

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,6 +1,7 @@
 import { randomIndex as random } from './random';
 
 export const shuffle = (array: any[]) => {
+  if (array.length <= 1) return array;
   let currentIndex = array.length - 1;
   let randomIndex;
   while (currentIndex !== 0) {
