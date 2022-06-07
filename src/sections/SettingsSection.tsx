39581848c9ef7f2c5cd4824204d6852be8981950
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-06-07 23:45:28 +0300

    Add voices info

--- src/sections/SettingsSection.tsx
+++ src/sections/SettingsSection.tsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useAsyncEffect from '../hoocks/useAsyncEffect';
+
+const getVoices = (): Promise<any[]> => {
+  return new Promise((resolve) => {
+    window.speechSynthesis.onvoiceschanged = (_e) => {
+      resolve(window.speechSynthesis.getVoices());
+    };
+    window.speechSynthesis.getVoices();
+  });
+};
 
 export default function SettingsSection() {
-  return <div>SettingsSection</div>;
+  const [voices, setVoices] = useState([]);
+  useAsyncEffect(async () => {
+    const voicesTry = window.speechSynthesis.getVoices();
+    const voices = voicesTry.length ? voicesTry : await getVoices();
+    if (voices) {
+      setVoices(voices);
+    }
+  }, []);
+  return (
+    <div>
+      SettingsSection
+      <h3>voices</h3>
+      <ul>
+        {voices.map((v, index) => (
+          <li key={v?.name}>
+            {index}
+            {'   '}
+            {v.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
