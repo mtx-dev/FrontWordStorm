0fe3ba92b85b48a77688839d6ee11b6c5f9ed586
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-07-18 22:10:41 +0300

    Remove useless field

--- src/models/ISettings.ts
+++ src/models/ISettings.ts
@@ -3,6 +3,5 @@ import { QuizNameType } from '../hoocks/useQuizes';
 export interface ISettings {
   voice: string | null;
   allowVioce: boolean;
-  allowedQuizes: string[];
-  quizes: QuizNameType[];
+  allowedQuizes: QuizNameType[];
 }
