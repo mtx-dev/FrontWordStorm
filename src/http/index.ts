5f283b34ac1e816c087f4f9c288739bc33a2b929
Andrey MT <andrey.kozlitskiy@gmail.com>
2022-07-25 23:25:01 +0300

    Fix error when response is fail

--- src/http/index.ts
+++ src/http/index.ts
@@ -16,7 +16,7 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
     if (
-      error.response.status === 401 &&
+      error.response?.status === 401 &&
       error.config &&
       !error.config._isTry &&
       error.config.url !== '/refresh'
